3c9bf4c163aaa9c262863e955b885e82024a4f53
Matt H <matthewehutton@gmail.com>
2022-03-02 16:04:49 +0000

    DP-2 #done

---
+++ application/__init__.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import uuid
+import os
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = str(uuid.uuid4()) # generating a random secret key at app startup
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URI")
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+from application import routes
