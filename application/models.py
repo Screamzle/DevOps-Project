c6778ccaac392f8222f2617e84f55ba676d3c5a0
Matt H <matthewehutton@gmail.com>
2022-03-04 11:01:16 +0000

    DP-6 #done models created and association table added

--- application/models.py
+++ application/models.py
@@ -1,25 +1,17 @@
 from flask_app import db
 from datetime import datetime
 
-# Join table for users and exercises
-workout_plans = db.Table('workout_plans', db.Model.metadata,
-    db.Column('exercise_ID', db.Integer, db.ForeignKey('exercises.exercise_ID')),
-    db.Column('user_ID', db.Integer, db.ForeignKey('users.user_ID'))
-)
-
 # Table schema for users
 class Users(db.Model):
     user_ID = db.Column(db.Integer, primary_key=True) # PK
+    user_name = db.Column(db.String(15), nullable=False, unique=True)# Username
+    password = db.Column(db.String(30), nullable=False) # Password
     first_name = db.Column(db.String(50), nullable=False) # First name
     last_name = db.Column(db.String(50), nullable=False) # Last name
     email_address = db.Column(db.String(75), nullable=False, unique=True) # Email address
-    user_name = db.Column(db.String(15), nullable=False, unique=True)# Username
-    password = db.Column(db.String(30), nullable=False) # Password
     date_created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow) # Date and time of account creation
-
-    # Define user relationship with exercises
-    exercises = db.relationship('Exercises', cascade = 'delete', backref = 'author', lazy = True)
-    workout_plans = db.relationship('Exercises', secondary = workout_plans, cascade = 'delete', backref = db.backref('workout_plans', lazy = 'dynamic'))
+    # creating relationship with Workout Plans
+    workout_plans = db.relationship('Workout_Plans', backref='user')
 
 # Table schema for exercises
 class Exercises(db.Model):
@@ -27,3 +19,12 @@ class Exercises(db.Model):
     exercise_name = db.Column(db.String(50), nullable=False, unique=True) # Exercise name
     repetitions = db.Column(db.Integer, nullable=False) # Number of repetitions
     sets = db.Column(db.Integer, nullable=False) # Number of sets
+    # creating relationship with Workout Plans
+    workout_plans = db.relationship('Workout_Plans', backref='exercise')
+
+# Table schema for workout_plans as association table
+class Workout_Plans(db.Model):
+    plan_id = db.Column(db.Integer, primary_key=True)
+    user_ID = db.Column('user_id', db.Integer, db.ForeignKey('users.user_ID'))
+    exercise_ID = db.Column('exercise_id', db.Integer, db.ForeignKey('exercises.exercise_ID'))
+    workout_name = db.Column('Workout_Name', db.String(30))
