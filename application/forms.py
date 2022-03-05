6b263d162e0360b40bd332a18dab068dca7fdda8
Matt H <matthewehutton@gmail.com>
2022-03-05 11:53:11 +0000

    DP-12 #in-progress forms added up to update/delete accounts

--- application/forms.py
+++ application/forms.py
@@ -45,3 +45,35 @@ class LogInForm(FlaskForm):
             DataRequired()
         ])
     submit = SubmitField('Sign in')
+
+# Update Account Form
+class UpdateAccountForm(FlaskForm):
+    user_name = StringField('User Name: ',
+        validators = [
+            DataRequired(),
+            Length(min = 3, max = 15)
+        ])
+    password = PasswordField('Password: ',
+        validators = [
+            DataRequired()
+        ])
+    first_name = StringField('First Name: ',
+        validators = [
+            DataRequired(),
+            Length(min = 3, max = 50)
+            ])
+    last_name = StringField('Last Name: ',
+        validators = [
+            DataRequired(),
+            Length(min = 2, max = 50)
+            ])
+    email_address = StringField('Email Address: ',
+        validators = [
+            DataRequired(),
+            Email()
+        ])
+    submit = SubmitField('Update Account Details')
+
+# Delete Account Form
+class DeleteAccountForm(FlaskForm):
+    delete = SubmitField('Delete Account')
