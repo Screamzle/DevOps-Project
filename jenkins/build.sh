583a0ee5cc75dc76151279c9e68cad371146036a
Matt <matthewehutton@gmail.com>
2022-03-08 22:27:25 +0000

    testing build

--- jenkins/build.sh
+++ jenkins/build.sh
@@ -2,5 +2,5 @@
 
 docker build -t screamzle/workoutapp:latest .
 docker tag screamzle/workoutapp:latest screamzle/workoutapp:$BUILD_NUMBER
-docker build -t sqldbimage db/.
+docker build -t screamzle/sqldbimage db/.
 docker tag screamzle/sqldbimage:latest screamzle/sqldbimage:$BUILD_NUMBER
