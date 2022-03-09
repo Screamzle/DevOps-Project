980861d2411a06a03d116915e8d0d7227e45386f
Matt <matthewehutton@gmail.com>
2022-03-09 01:02:05 +0000

    exposed port 5000 in run command

--- jenkins/deploy.sh
+++ jenkins/deploy.sh
@@ -2,8 +2,7 @@
 
 if docker inspect  sqldb_container > /dev/null 2>&1 ; then docker stop sqldb_container; docker rm sqldb_container; fi
 docker run -d --network my-network --name sqldb_container screamzle/sqldbimage
-sleep 10
 if docker inspect  workoutapp_container > /dev/null 2>&1 ; then docker stop workoutapp_container; docker rm workoutapp_container; fi
-docker run -d --network my-network --name workoutapp_container screamzle/workoutapp
+docker run -d -p 5000:5000 --network my-network --name workoutapp_container screamzle/workoutapp
 if docker inspect  nginx_container > /dev/null 2>&1 ; then docker stop nginx_container; docker rm nginx_container; fi
 docker run -d --network my-network --name nginx_container -p 80:80 --mount type=bind,src=$WORKSPACE/nginx/nginx.conf,dst=/etc/nginx/nginx.conf nginx
