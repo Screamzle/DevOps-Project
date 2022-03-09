4317635f2de95448c0734815c9214ecbc224d728
Matt <matthewehutton@gmail.com>
2022-03-09 00:52:23 +0000

    EOD

--- jenkins/build.sh
+++ jenkins/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker build -t screamzle/workoutapp:latest application/.
+docker build -t screamzle/workoutapp:latest .
 docker tag screamzle/workoutapp:latest screamzle/workoutapp:$BUILD_NUMBER
 docker build -t screamzle/sqldbimage db/.
 docker tag screamzle/sqldbimage:latest screamzle/sqldbimage:$BUILD_NUMBER
